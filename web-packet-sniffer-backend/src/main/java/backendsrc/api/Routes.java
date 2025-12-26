5e2c2aeee87b70d7ec0dff2e077c05e2103cf230
ardeng234 <arden.gourlay@outlook.com>
2025-12-26 20:24:39 +1100

    basic interface return api

--- web-packet-sniffer-backend/src/main/java/backendsrc/api/Routes.java
+++ web-packet-sniffer-backend/src/main/java/backendsrc/api/Routes.java
@@ -1,13 +1,26 @@
 package backendsrc.api;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import backendsrc.service.CaptureService;
+import backendsrc.service.NetworkInterfaceInfo;
 
 @RestController
 public class Routes {
     @GetMapping("/api/hello")
     public String hello() {
         return "Hello from Spring Boot!";
+    }
+
+     @GetMapping("/api/interfaces")
+    public List<String> interfaces() {
+        CaptureService service = new CaptureService(0);
+        List<NetworkInterfaceInfo> interfaces = service.getNetworkInterfaces();
+        List<String> returnedInterfaces = interfaces.stream().map(NetworkInterfaceInfo::toString).collect(Collectors.toList()); 
+        return returnedInterfaces;
     }
 
 }
