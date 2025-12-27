8644c47950573e4f396772dc8aaffea00acd076a
ardeng234 <arden.gourlay@outlook.com>
2025-12-27 19:04:08 +1100

    implemented classes for return objects

--- web-packet-sniffer-backend/src/main/java/backendsrc/api/Routes.java
+++ web-packet-sniffer-backend/src/main/java/backendsrc/api/Routes.java
@@ -1,26 +1,31 @@
 package backendsrc.api;
 
 import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
 import backendsrc.service.CaptureService;
 import backendsrc.service.NetworkInterfaceInfo;
 
 @RestController
 public class Routes {
+    CaptureService service = new CaptureService(1000);
     @GetMapping("/api/hello")
     public String hello() {
         return "Hello from Spring Boot!";
     }
 
-     @GetMapping("/api/interfaces")
-    public List<String> interfaces() {
-        CaptureService service = new CaptureService(0);
+    @GetMapping(value = "/api/interfaces", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<NetworkInterfaceInfo> interfaces() {
         List<NetworkInterfaceInfo> interfaces = service.getNetworkInterfaces();
-        List<String> returnedInterfaces = interfaces.stream().map(NetworkInterfaceInfo::toString).collect(Collectors.toList()); 
-        return returnedInterfaces;
+        return interfaces;
     }
+
+    @GetMapping(value = "/api/capture/start", produces = MediaType.APPLICATION_JSON_VALUE)
+    public CaptureStatusResponse startCapture(String interfaceName) {
+        service.startCapture(interfaceName);
+        CaptureStatusResponse res = new CaptureStatusResponse(service.getSessionID(),service.getSessionState());
+        return res;
+    } 
 
 }
