6a0f1e61986635036b5130c35a4fcff733a66b7a
ardeng234 <arden.gourlay@outlook.com>
2025-12-27 23:42:08 +1100

    exception handler for CaptureEngine errors

--- web-packet-sniffer-backend/src/main/java/backendsrc/api/ApiErrorHandler.java
+++ web-packet-sniffer-backend/src/main/java/backendsrc/api/ApiErrorHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import backendsrc.service.exception.CaptureEngineException;
 import backendsrc.service.exception.CaptureSessionStateInvalidException;
 import backendsrc.service.exception.InterfaceNotFoundException;
 
@@ -12,14 +13,20 @@ import backendsrc.service.exception.InterfaceNotFoundException;
 public class ApiErrorHandler {
     @ExceptionHandler(InterfaceNotFoundException.class)
     public ResponseEntity<ApiErrorResponse> handleInterfaceNotFound(InterfaceNotFoundException ex) {
-            ApiErrorResponse errorResponse = new ApiErrorResponse(HttpStatus.NOT_FOUND.value(),"Not Found", ex.getMessage());
-            return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+            ApiErrorResponse errorResponse = new ApiErrorResponse(HttpStatus.BAD_REQUEST.value(),"Not Found", ex.getMessage());
+            return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(CaptureSessionStateInvalidException.class)
     public ResponseEntity<ApiErrorResponse> handleCaptureAlreadyRunning(CaptureSessionStateInvalidException ex) {
             ApiErrorResponse errorResponse = new ApiErrorResponse(HttpStatus.CONFLICT.value(),"Conflict", ex.getMessage());
             return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(CaptureEngineException.class)
+    public ResponseEntity<ApiErrorResponse> handleCaptureEngineError(CaptureEngineException ex) {
+            ApiErrorResponse errorResponse = new ApiErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(),"Conflict", ex.getMessage());
+            return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
     
 }
